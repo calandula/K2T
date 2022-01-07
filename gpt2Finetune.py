8d42af6488d907c718090265395784c7e12737da
calandula <calandula97@gmail.com>
2022-01-07 15:26:27 +0100

    sequence treatment

--- gpt2Finetune.py
+++ gpt2Finetune.py
@@ -22,24 +22,26 @@ model = AutoModelWithLMHead.from_pretrained("gpt2")
 
 train_path = './data/guide_context/'
 
-data_names = ["FootballCommentariesGoals.jsonl", "FootballCommentariesRedCards.jsonl", "FootballCommentariesSubs.jsonl", "FootballCommentariesYellowCards.jsonl"]
+train_names = ["FootballCommentariesGoalsTrain.jsonl", "FootballCommentariesRedCardsTrain.jsonl", "FootballCommentariesSubsTrain.jsonl", "FootballCommentariesYellowCardsTrain.jsonl"]
+test_names = ["FootballCommentariesGoalsTest.jsonl", "FootballCommentariesRedCardsTest.jsonl", "FootballCommentariesSubsTest.jsonl", "FootballCommentariesYellowCardsTest.jsonl"]
 
-data_files = [train_path + file_name for file_name in data_names]
+data_files_train = [train_path + file_name for file_name in train_names]
+data_files_test = [train_path + file_name for file_name in test_names]
 
-dataset = load_dataset('json', data_files=data_files)
+dataset = load_dataset('json', data_files=data_files_train, split='train[:100]+validation[:100]')
 
-training_args = TrainingArguments(
-    output_dir="./GPT2Finetuned",
-    overwrite_output_dir=True,
-    num_train_epochs=10,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    gradient_accumulation_steps=4,
-    prediction_loss_only=True
-)
-
-torch.cuda.empty_cache()
-model.to('cuda')
+# training_args = TrainingArguments(
+#     output_dir="./GPT2Finetuned",
+#     overwrite_output_dir=True,
+#     num_train_epochs=10,
+#     per_device_train_batch_size=16,
+#     per_device_eval_batch_size=16,
+#     gradient_accumulation_steps=4,
+#     prediction_loss_only=True
+# )
+#
+# torch.cuda.empty_cache()
+# model.to('cuda')
 
 
 
