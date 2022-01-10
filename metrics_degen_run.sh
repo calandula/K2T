c91240b9f12269fc6c112febae523bbba093446f
calandula <adria.espejo@est.fib.upc.edu>
2022-01-10 23:05:12 +0100

    ch

--- metrics_degen_run.sh
+++ metrics_degen_run.sh
@@ -2,7 +2,11 @@
 
 # Change path (-file=) to the file for which you want to calculate the metrics
 
-python metrics_degen2.py -file='./results/guide_context/tests/Results_Model_gpt2-large_nBeams_4_guideContext_True_.txt' -n_sample=30
-python metrics_degen2.py -file='./results/guide_context/tests2/Results_Model_gpt2_nBeams_4_guideContext_True_.txt' -n_sample=30
-python metrics_degen2.py -file='./results/guide_context/tests3/Results_Model_gpt2-large_nBeams_4_guideContext_False_.txt' -n_sample=30
-python metrics_degen2.py -file='./results/guide_context/tests4/Results_Model_gpt2-large_nBeams_4_guideContext_False_.txt' -n_sample=30
+#python metrics_degen2.py -file='./results/guide_context/tests/Results_Model_gpt2-large_nBeams_4_guideContext_True_.txt' -n_sample=30
+#python metrics_degen2.py -file='./results/guide_context/tests2/Results_Model_gpt2_nBeams_4_guideContext_True_.txt' -n_sample=30
+#python metrics_degen2.py -file='./results/guide_context/tests3/Results_Model_gpt2-large_nBeams_4_guideContext_False_.txt' -n_sample=30
+#python metrics_degen2.py -file='./results/guide_context/tests4/Results_Model_gpt2-large_nBeams_4_guideContext_False_.txt' -n_sample=30
+python metrics_degen2.py -file='./results/guide_context/tests5/Results_Model_gpt2-large_nBeams_4_guideContext_True_contextWeight_2.0_.txt' -n_sample=30
+python metrics_degen2.py -file='./results/guide_context/tests5/Results_Model_gpt2-large_nBeams_4_guideContext_True_contextWeight_3.0_.txt' -n_sample=30
+python metrics_degen2.py -file='./results/guide_context/tests5/Results_Model_gpt2-large_nBeams_4_guideContext_True_contextWeight_4.0_.txt' -n_sample=30
+python metrics_degen2.py -file='./results/guide_context/tests5/Results_Model_gpt2-large_nBeams_4_guideContext_True_contextWeight_5.0_.txt' -n_sample=30
--- run.sh
+++ run.sh
@@ -6,10 +6,13 @@
 # python main2.py -mode='next' -file_name='/data/guide_context/30KWFOOTBALL.txt' -results_subfolder=tests2 -weight='5.0' -top_p='0.9' -n_generated_sentences=20 -n_beams=4 -do_guarantee=True -guide_context=True -model=gpt2
 # python main2.py -mode='next' -file_name='/data/guide_context/30KWFOOTBALL.txt' -results_subfolder=tests3 -weight='5.0' -top_p='0.9' -n_generated_sentences=20 -n_beams=4 -do_guarantee=True
 # python main2.py -mode='next' -file_name='/data/guide_context/30KWOFOOTBALL.txt' -results_subfolder=tests4 -weight='5.0' -top_p='0.9' -n_generated_sentences=20 -n_beams=4 -do_guarantee=True
- python main2.py -mode='next' -file_name='/data/guide_context/30KWFOOTBALL.txt' -results_subfolder=tests5 -weight='5.0' -top_p='0.9' -n_generated_sentences=20 -n_beams=4 -do_guarantee=True -guide_context=True -context_weight=2.
- python main2.py -mode='next' -file_name='/data/guide_context/30KWFOOTBALL.txt' -results_subfolder=tests5 -weight='5.0' -top_p='0.9' -n_generated_sentences=20 -n_beams=4 -do_guarantee=True -guide_context=True -context_weight=3.
- python main2.py -mode='next' -file_name='/data/guide_context/30KWFOOTBALL.txt' -results_subfolder=tests5 -weight='5.0' -top_p='0.9' -n_generated_sentences=20 -n_beams=4 -do_guarantee=True -guide_context=True -context_weight=4.
- python main2.py -mode='next' -file_name='/data/guide_context/30KWFOOTBALL.txt' -results_subfolder=tests5 -weight='5.0' -top_p='0.9' -n_generated_sentences=20 -n_beams=4 -do_guarantee=True -guide_context=True -context_weight=5.
+# python main2.py -mode='next' -file_name='/data/guide_context/30KWFOOTBALL.txt' -results_subfolder=tests5 -weight='5.0' -top_p='0.9' -n_generated_sentences=20 -n_beams=4 -do_guarantee=True -guide_context=True -context_weight=2.
+# python main2.py -mode='next' -file_name='/data/guide_context/30KWFOOTBALL.txt' -results_subfolder=tests5 -weight='5.0' -top_p='0.9' -n_generated_sentences=20 -n_beams=4 -do_guarantee=True -guide_context=True -context_weight=3.
+# python main2.py -mode='next' -file_name='/data/guide_context/30KWFOOTBALL.txt' -results_subfolder=tests5 -weight='5.0' -top_p='0.9' -n_generated_sentences=20 -n_beams=4 -do_guarantee=True -guide_context=True -context_weight=4.
+# python main2.py -mode='next' -file_name='/data/guide_context/30KWFOOTBALL.txt' -results_subfolder=tests5 -weight='5.0' -top_p='0.9' -n_generated_sentences=20 -n_beams=4 -do_guarantee=True -guide_context=True -context_weight=5.
+  python main2.py -mode='next' -file_name='/data/guide_context/30KWFOOTBALL.txt' -results_subfolder=tests5 -weight='5.0' -top_p='0.9' -n_generated_sentences=20 -n_beams=4 -do_guarantee=True -guide_context=True -context_weight=6.
+  python main2.py -mode='next' -file_name='/data/guide_context/30KWFOOTBALL.txt' -results_subfolder=tests5 -weight='5.0' -top_p='0.9' -n_generated_sentences=20 -n_beams=4 -do_guarantee=True -guide_context=True -context_weight=7.
+
 
 ###### END
 
